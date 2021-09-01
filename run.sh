dffd4ab08af271cf9b1af9bc4de1c845827f7cf6
xud@aws <xud@graphcore.ai>
2021-09-01 09:41:16 +0000

    change for test, update run.sh since script is not supported in .sh script

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
-dt=$(date '+%d-%m-%Y_%H-%M-%S')
-script logs/log_$dt.txt
-
 CUDA_VISIBLE_DEVICES=0,1 python train_semantic_softmax.py \
---batch_size=8 \
---data_path=/home/ubuntu/data/imagenet10k/imagenet21k_resized_new \
---model_name=vit_base_patch16_224 \
---num_classes=10450 \
---epochs=300
-
-exit
+    --batch_size=8 \
+    --data_path=/home/ubuntu/data/test21k \
+    --model_name=vit_base_patch16_224 \
+    --num_classes=19167 \
+    --epochs=300
