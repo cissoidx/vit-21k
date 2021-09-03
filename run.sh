6d18df6e7a6eccdcdc6b756d1290c1016cda3dc2
xud@aws <xud@graphcore.ai>
2021-09-03 08:04:56 +0000

    modify num workers

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 python -u -m torch.distributed.launch --nproc_per_node=8 train_semantic_softmax.py \
     --batch_size=8 \
+    --num_workers=16 \
     --data_path=/home/ubuntu/data/imagenet10k/imagenet21k_resized_new \
     --model_name=vit_base_patch16_224 \
     --num_classes=10450 \
