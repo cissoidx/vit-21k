72c0a270a23a25d1f0d2374edbb6a75025a91a19
xud@aws <xud@graphcore.ai>
2021-09-03 07:58:13 +0000

    modify number of classes

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ python -u -m torch.distributed.launch --nproc_per_node=8 train_semantic_softmax.
     --batch_size=8 \
     --data_path=/home/ubuntu/data/imagenet10k/imagenet21k_resized_new \
     --model_name=vit_base_patch16_224 \
-    --num_classes=19167 \
+    --num_classes=10450 \
     --epochs=300
