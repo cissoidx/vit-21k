d16f593ce4be730df8143608e687557d149e53c9
xud@aws <xud@graphcore.ai>
2021-09-03 07:21:35 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0,1 python train_semantic_softmax.py \
+python -u -m torch.distributed.launch --nproc_per_node=8 train_semantic_softmax.py \
     --batch_size=8 \
-    --data_path=/home/ubuntu/data/test21k \
+    --data_path=/home/ubuntu/data/imagenet10k/imagenet21k_resized_new \
     --model_name=vit_base_patch16_224 \
     --num_classes=19167 \
     --epochs=300
